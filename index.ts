7d617409c4bab42b7b874b05dbcd11b2f9224d1a
vahid <vahid.hossaini@gmail.com>
2023-08-20 10:33:40 +0330

    Init

---
+++ index.ts
@@ -0,0 +1,5 @@
+import CommonService from "./src/commonService";
+import ValidationService from "./src/validationService";
+import WebService from "./src/webService";
+
+export {WebService,CommonService,ValidationService}
